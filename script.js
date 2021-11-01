1888e53fcb9a02726102787deb8c0548438e6caf
AlishSafarli <57995570+AlishSafarli@users.noreply.github.com>
2021-11-01 16:02:44 +0400

    'slice img into parts'

--- script.js
+++ script.js
@@ -1,19 +1,48 @@
-let img = document.getElementById('img2');
-let can = document.getElementById('can');
+let img = new Image(); // your img elements have a width of 500 from css, but the image data is much larger. we want to work with raw image here
+let can = document.getElementById('myCan');
 let ctx = can.getContext('2d');
-
+let can2 = document.getElementById('myCan2');
+let ctx2 = can2.getContext('2d');
+let canImage = new Image();
+canImage.id = "pic";
 img.onload = function () {
+
 	let iw = img.width;
 	let ih = img.height;
 
-	can.width = iw;
-	can.height = ih;
+	let iw2 = canImage.width;
+	let ih2 = canImage.height;
+	if (iw > 1000) {
+		can.style.width = 400;
+		resizeImg(img);
+	}
+	else {
+		can.width = iw * 1.2; //making canvas a bit wider so that the spacing will fit inside it
+		can.height = ih;
 
-	for (let i = 0; i <= 3; i++) {
-
-		ctx.drawImage(img, 0, 0, iw, ih, 0, 0, iw / i, ih);
+		const sliceWidth = iw / 3;
+		const spacing = iw * 0.1;
+		for (let i = 0; i < 3; i++) {
+			ctx.drawImage(img, sliceWidth * i, 0, sliceWidth, ih, (sliceWidth + spacing) * i, 0, sliceWidth, ih);
+		}
 	}
 
 }
-img.src = "https://mralish.com/imgs/me_watch.jpg"
+function resizeImg(pic) {
+	let resizingPicW = pic.width;
+	resizingPicW = 500;
+	let resizingPicH = resizingPicW / 1.5;
+	can2.width = resizingPicW;
+	can2.height = resizingPicH;
+	ctx2.drawImage(img, 0, 0, resizingPicW, resizingPicH);
+}
+//I want to take that pic but it is empty
+const dataURI = can2.toDataURL();
+
+img.src = "https://mralish.com/imgs/me_watch.jpg";
+resizeImg(img);
+document.getElementById('myImg').src = dataURI;
+//img.src = "dog.jpg";
+// img.width = 600;
+// img.height = 250;
 
